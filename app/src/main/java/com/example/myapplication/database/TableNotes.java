d45e4baa644ec6942347f48d4b7e8e09c806c3f4
JhostinD <jhostin2906@gmail.com>
2023-04-30 20:34:23 +0200

    added delete notes

--- app/src/main/java/com/example/myapplication/database/TableNotes.java
+++ app/src/main/java/com/example/myapplication/database/TableNotes.java
@@ -2,9 +2,12 @@ package com.example.myapplication.database;
 
 import com.example.myapplication.model.Note;
 
+import java.util.Collection;
 import java.util.List;
 
 public interface TableNotes {
     long insertData(Note note, String name);
     List<Note> obtainNotes();
+    Note deleteNote(String title);
+    Collection<Note> obtainAllNotes();
 }
